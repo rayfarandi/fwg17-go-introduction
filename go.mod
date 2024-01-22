4cad67ee57c6b195b7846851c3edd7c991e12a33
Ran Ran <rayfarandi1994@gmail.com>
2024-01-22 20:13:07 +0700

    tugas 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rayfarandi/fwg17-go-introductionndi/fwg17-go-introduction
+
+go 1.21.6
---
+++ task1/task1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func printSegitiga(tinggi int) {
+	for i := tinggi; i > 0; i-- {
+		for j := 0; j < tinggi-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 0; k < 2*i-1; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	baris := 5
+	printSegitiga(baris)
+}
